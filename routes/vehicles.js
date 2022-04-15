bd04feb0034902707f2b2e0b4cdd75a5df5f66e8
ShannaSmith <shannagjh@gmail.com>
2022-04-15 11:19:37 -0400

    remove add service link from service index page

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const vehiclesCtrl = require('../controllers/vehicles');
 
+router.post('/', vehiclesCtrl.create);
 router.get('/', vehiclesCtrl.index);
 router.get('/new', vehiclesCtrl.new);
 router.get('/:id', vehiclesCtrl.show);
-router.post('/', vehiclesCtrl.create);
 router.post('/:id', vehiclesCtrl.destroy);
 module.exports = router;
